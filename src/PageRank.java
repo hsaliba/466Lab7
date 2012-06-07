76549ee33c9601eff92240cfa2bf609d7ab8b5be
hsaliba <hsaliba@calpoly.edu>
2012-06-06 21:08:26 -0700

    asd

--- src/PageRank.java
+++ src/PageRank.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class PageRank {
+   private List<List<Integer>> graph = new ArrayList<List<Integer>> ();
+   private List<String> nodes = new ArrayList<String> ();
+   
    public static void main(String[] args) {
-      
+      if(args.length != 2) {
+         System.out.println("usage: PageRank <filename.csv>");
+         System.exit(-1);
+      }
+      parseGraphs();
 
+   }
+
+   private static void parseGraphs() {
+      
+      
    }
 
 }
