8fff2c147b49f6ef6b0d341c0a87765110a72359
skrapil <skrapil@calpoly.edu>
2012-06-06 23:52:13 -0700

    ljlk

--- src/PageRank.java
+++ src/PageRank.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class PageRank {
    private static List<List<Integer>> graph = new ArrayList<List<Integer>> ();   //get(from).get(to)
    private static List<String> nodes = new ArrayList<String> ();               //List of names, same index across horizontal/vertical sides of matrix. same length as graph.size()
-   private static List<Double> pageRanks = new ArrayList<Double>();
+   private static double[] pageRanks;
       
    public static void main(String[] args) {
       if(args.length != 1) {
@@ -22,6 +22,10 @@ public class PageRank {
          System.exit(-1);
       }
 
+      System.out.println(graph);
+
+      pageRanks = new double[graph.size()];
+      System.out.println(pageRank(1));
    }
 
    private static void parseGraphs(String file) throws FileNotFoundException {
@@ -101,33 +105,16 @@ public class PageRank {
       return count;
    }
 
-   public static double getPageRank(int ndx) {
-      double d = 0.0;
-      double pr = 0.0;
-      double ppr = 0.0;
-
-
-      d = (double)getIn(ndx)/graph.size();
-
-      for (int i = 0; i < getOut(ndx); i++) {
-      //while diff is large enough
-      //for all incoming pages
-      // get page rank
-      // mult pr by 1/#out
-      //sum of ^
-       }   
-
-      
-      return 0.0;
-   }
-
    //d = prob of clicking on any links on page
    private static double pageRank(int ndx) {
       double pr = 0.0;
       double sum = 0.0;
       ArrayList<Integer> in = new ArrayList<Integer>();
-      double d = (double)getOut(in.get(ndx))/graph.size();//maybe not size
+      double d = (double)getOut(ndx)/graph.size();//maybe not size
       ArrayList<Double> pprs = new ArrayList<Double>();
+
+      if (stop())
+         return (double)1/graph.size();
 
       //find all in 
       for (int i = 0; i < graph.size(); i++) {
@@ -143,23 +130,33 @@ public class PageRank {
 
       pr = (1-d)*(1/(double)graph.size()) + d * sum;
 
-      sum = 0.0;
-      for (int i = 0; i < pprs.size(); i++) {
-      }
-      return 0;
+      return pr;
    }
 
    private static boolean stop() {
+      double sum = 0.0;
 
-      if (pageRanks.size() < graph.size()) 
-         return false;
+      for (int i = 0; i < pageRanks.length; i++) 
+         if (Double.compare(pageRanks[i], 0) == 0)
+            return false;
       
-      for (int i = 0; i < pageRanks.size(); i++) {
-
-
-
+      for (int i = 0; i < pageRanks.length; i++) {
+         ArrayList<Integer> in = new ArrayList<Integer>();
+         for (int j = 0; j < graph.size(); j++) {
+            if (graph.get(j).get(i) == 1) 
+               in.add(j);
+          } 
+         
+          for (int j = 0; j < in.size(); j++) {
+             sum += pageRanks[i] - pageRanks[in.get(j)];  
+          }
       }
-   return false;
+
+      System.out.println("e: "+sum);
+      if (sum < 10)
+         return true; 
+
+      return false;
    } 
    
 }
